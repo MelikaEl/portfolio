37971b5e8fcd4c0b9fe40f31e67c57d74678d93e
MelikaEl <melika.melika8@gmail.com>
2024-08-27 10:41:09 +0430

    add image slider for pictures of projects

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+document.querySelectorAll('.slider').forEach(slider => {
+    const images = slider.querySelectorAll('img');
+    let currentIndex = 0;
+  
+    // Function to show the next image
+    function showNextImage() {
+      images[currentIndex].classList.remove('active');
+      currentIndex = (currentIndex + 1) % images.length;
+      images[currentIndex].classList.add('active');
+    }
+  
+    // Set the first image to be visible
+    images[currentIndex].classList.add('active');
+  
+    // Change image every 3 seconds
+    setInterval(showNextImage, 3000);
+  });
